dbc604582de5aa7c87bd691a0f36bdf0e1c58c8a
Fabricio <fabricioliquesley15@gmail.com>
2024-01-19 09:53:38 -0300

    feat: created AppError

--- src/server.js
+++ src/server.js
@@ -1,11 +1,32 @@
 require("dotenv/config");
+require("express-async-errors");
+
+const AppError = require("./utils/AppError");
+
 const express = require("express");
 
 const app = express();
+app.use(express.json());
 
 const routes = require("./routes");
 
 app.use(routes);
+
+app.use((error, request, response, next) => {
+    if(error instanceof AppError){
+        return response.status(error.statusCode).json({
+            status: "error",
+            message: error.message
+        });
+    }
+
+    console.error(error);
+
+    return response.status(500).json({
+        status: "error",
+        message: "Internal Server Error"
+    });
+})
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server listening on ${PORT}`));
