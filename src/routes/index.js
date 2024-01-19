9d826b27f15b822ae78767553566376e7f7bd083
Fabricio <fabricioliquesley15@gmail.com>
2024-01-19 19:44:23 -0300

    feat: created the session controller to create a session for the user

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,11 @@
 const { Router } = require("express");
 
 const userRoutes = require("./user.routes");
+const sessionsRoute = require("../routes/sessions.routes");
 
 const routes = Router();
 
 routes.use("/users", userRoutes);
+routes.use("/sessions", sessionsRoute);
 
 module.exports = routes;
